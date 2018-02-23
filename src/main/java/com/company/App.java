82fe98204cd3d68affdeeb0a402fe18891d9eda2
JeffRisberg <jrisberg@aol.com>
2018-02-23 15:53:58 -0800

    added properties

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -2,6 +2,9 @@ package com.company;
 
 import org.flywaydb.core.Flyway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Example application
  */
@@ -15,8 +18,17 @@ public class App {
         String tenantNamespace = "10000";
         ConnectionFactory.setupDataSource(flyway, tenantNamespace);
 
+        Map<String, String> properties = new HashMap<String, String>();
+
+        properties.put("flyway.driver", "com.mysql.jdbc.Driver");
+        properties.put("flyway.url", "jdbc:mysql://localhost:3306/flyway01?autoreconnect=true");
+        properties.put("flyway.user", "developer");
+        properties.put("flyway.password", "123456");
+
+        flyway.configure(properties);
+
         // Point it to the database
-        flyway.setDataSource("jdbc:h2:file:./target/foobar", "sa", null);
+        //flyway.setDataSource("jdbc:h2:file:./target/foobar", "sa", null);
 
         // Start the migration
         flyway.migrate();
